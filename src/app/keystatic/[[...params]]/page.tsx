9d692a508cdf69cf2598e160bc405b8e9e3b9c1e
Lawrence Nwuzor <chigozienwuzor2018@gmail.com>
2026-05-19 08:19:17 -0400

    fix: wrap Keystatic admin in white background container

--- src/app/keystatic/[[...params]]/page.tsx
+++ src/app/keystatic/[[...params]]/page.tsx
@@ -3,4 +3,12 @@ export const dynamic = "force-dynamic";
 import { makePage } from "@keystatic/next/ui/app";
 import config from "../../../../keystatic.config";
 
-export default makePage(config);
+const KeystaticPage = makePage(config);
+
+export default function Page() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#fff", color: "#000" }}>
+      <KeystaticPage />
+    </div>
+  );
+}
